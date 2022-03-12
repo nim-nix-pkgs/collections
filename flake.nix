65558a3c3c638c972f6980e695e07b4ec7d2aed3
hugosenari <hugosenari@gmail.com>
2022-03-12 01:17:46 -0300

    chore: re index collections

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = ''Various collections and utilities'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."collections-master".url = "path:./master";
+  inputs."collections-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."collections-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."collections-v0_0_2".url = "path:./v0_0_2";
+  inputs."collections-v0_0_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."collections-v0_0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."collections-v0_1_2".url = "path:./v0_1_2";
+  inputs."collections-v0_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."collections-v0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."collections-v0_3_0".url = "path:./v0_3_0";
+  inputs."collections-v0_3_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."collections-v0_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."collections-v0_3_1".url = "path:./v0_3_1";
+  inputs."collections-v0_3_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."collections-v0_3_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."collections-v0_3_2".url = "path:./v0_3_2";
+  inputs."collections-v0_3_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."collections-v0_3_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."collections-v0_3_3".url = "path:./v0_3_3";
+  inputs."collections-v0_3_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."collections-v0_3_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."collections-v0_3_4".url = "path:./v0_3_4";
+  inputs."collections-v0_3_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."collections-v0_3_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."collections-v0_5_0".url = "path:./v0_5_0";
+  inputs."collections-v0_5_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."collections-v0_5_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."collections-v0_5_1".url = "path:./v0_5_1";
+  inputs."collections-v0_5_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."collections-v0_5_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."collections-v0_5_2".url = "path:./v0_5_2";
+  inputs."collections-v0_5_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."collections-v0_5_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Various collections and utilities'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-collections-master.flake = false;
+  inputs.src-collections-master.owner = "zielmicha";
+  inputs.src-collections-master.ref   = "refs/heads/master";
+  inputs.src-collections-master.repo  = "collections.nim";
+  inputs.src-collections-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-collections-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-collections-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
